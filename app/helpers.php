896807b2a85f5eaa5ce6beb99264720389423be8
miltonzini <miltonzini@gmail.com>
2025-04-12 17:21:33 -0300

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -10,11 +10,33 @@ function setActiveRoute($routeName)
 
 
 
-function optimizeImage ($image) {
+function optimizeImage($image, $isAlreadyWebp = false) {
     if ($image->width() > 1920 || $image->height() > 1080) {
         $image->scale(width: 1080); 
     }
 
-    $image = $image->toWebp(80);
+    if (!$isAlreadyWebp) {
+        $image = $image->toWebp(80);
+    }
+    return $image;
+}
+
+function optimizeImageThumb($image, $isAlreadyWebp = false) {
+    if ($image->width() > 300 || $image->height() > 300) {
+        $image->scale(width: 300);
+    }
+    if (!$isAlreadyWebp) {
+        $image = $image->toWebp(80);
+    }
+    return $image;
+}
+
+function optimizeImageMedium($image, $isAlreadyWebp = false) {
+    if ($image->width() > 800 || $image->height() > 600) {
+        $image->scale(width: 800);
+    }
+    if (!$isAlreadyWebp) {
+        $image = $image->toWebp(80);
+    }
     return $image;
 }
