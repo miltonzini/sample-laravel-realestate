225c2a1634a25a420086c0f9dbb376feba04b364
miltonzini <miltonzini@gmail.com>
2025-04-13 14:59:05 -0300

    feat(post): add model and migration

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'short_description',
+        'body',
+        'button_text',
+        'button_url',
+        'author',
+        'status'
+    ];
+
+}
---
+++ database/migrations/2025_04_13_175627_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('short_description')->nullable();
+            $table->longText('body');
+            $table->string('button_text')->nullable();
+            $table->string('button_url')->nullable();
+            $table->string('author');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
