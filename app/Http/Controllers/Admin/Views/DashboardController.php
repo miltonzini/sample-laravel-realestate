72b00ec6e505a4c7de832d3623b8772d5a45017f
miltonzini <miltonzini@gmail.com>
2025-04-13 19:43:27 -0300

    feat: show misc info in dashboard home view

--- app/Http/Controllers/Admin/Views/DashboardController.php
+++ app/Http/Controllers/Admin/Views/DashboardController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\Admin\Views;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Property;
+use App\Models\Development;
+use App\Models\User;
+use App\Models\Post;
 
 class DashboardController extends Controller
 {
     public function showDashboard() {
-        $scripts = [''];
-        return view('admin.dashboard', compact('scripts'));
+        $propertiesCount = Property::count();
+        $developmentsCount = Development::count();
+        $postsCount = Post::count();
+        $usersCount = User::count();
+
+        $scripts = ['dashboard-misc.js'];
+        return view('admin.dashboard', compact('scripts', 'propertiesCount', 'developmentsCount', 'postsCount', 'usersCount'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,25 +9,25 @@
     @include('partials.admin.nav')
 
     <div class="admin-main-content">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, placeat nobis dolore quasi nesciunt dolor maxime quis harum perspiciatis id quos odit illum minima numquam error culpa nihil aut assumenda!</p>
+        <h2 class="mb5">Resumen</h2>
+        <div class="row dashboard-summary-section">
+            <a class="item rounded-4" href="{{ route('admin.properties.index') }}">
+                <div class="number">{{ $propertiesCount }}</div>
+                <div class="title">Propiedades</div>
+            </a>
+            <a class="item rounded-4" href="{{ route('admin.developments.index') }}">
+                <div class="number">{{ $developmentsCount }}</div>
+                <div class="title">Emprendimientos</div>
+            </a>
+            <a class="item rounded-4" href="{{ route('admin.blog.index') }}">
+                <div class="number">{{ $postsCount }}</div>
+                <div class="title">Posts</div>
+            </a>
+            <a class="item rounded-4" href="{{ route('admin.users.index') }}">
+                <div class="number">{{ $usersCount }}</div>
+                <div class="title">Usuarios registrados</div>
+            </a>
+        </div>
     </div>
 </main>
 @push('scripts')
