c682a0689c271a4ec4a13ce301e5296812436a6b
miltonzini <miltonzini@gmail.com>
2025-04-13 21:23:05 -0300

    feat(post): add accessors for formatted title, excerpt and author name

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,4 +29,26 @@ class Post extends Model
         return $this->belongsTo(User::class, 'author');
     }
 
+
+    public function getShortTitleAttribute()
+    {
+        if (strlen($this->title) >= 50) {
+            return substr($this->title, 0, 47) . '...';
+        }
+        return $this->title;
+    }
+
+    public function getExcerptAttribute()
+    {
+        if ($this->short_description) {
+            return $this->short_description;
+        }
+        
+        return substr(strip_tags($this->body), 0, 150) . '...';
+    }
+
+    public function getAuthorFullNameAttribute()
+    {
+        return $this->author ? $this->author()->first()?->name . ' ' . $this->author()->first()?->surname : 'Desconocido';
+    }
 }
