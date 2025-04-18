71e02606be3d95c78ccc5e48f4baf54e291aae58
miltonzini <miltonzini@gmail.com>
2025-04-18 18:28:17 -0300

    chore: fix pagination styles
    -publish pagination views
    -update css
    -update default pagination view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('assetVersionQueryString', '?v=' . env('ASSET_VERSION', '1'));
+
+        Paginator::defaultView('pagination::default');
+        Paginator::defaultSimpleView('pagination::default');
     }
 }
